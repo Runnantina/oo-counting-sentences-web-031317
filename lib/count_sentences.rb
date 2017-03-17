ffb6438a9e0df1dd54da0cd7858afeea43bca4e6
Runnan Zhong <r.tina.zhong@gmail.com>
2017-03-17 18:42:54 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
+
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
+    # return an integer
+    count = 0
+    array = self.split(' ')
+    array.each do |element|
+      if element.sentence? || element.question? || element.exclamation?
+        count += 1
+      end
 
+    end
+    count
   end
+
 end
